32350e640cbfc217427c1a37b0be34e2c48a9cf0
Jamie <jamie.danielle@denverrollerderby.org>
2023-03-24 15:32:15 -0600

    returned largest number in function_d

--- function_d.py
+++ function_d.py
@@ -2,7 +2,11 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
+    largest_number = 0
+    for num in numbers:
+        if num >= largest_number:
+            largest_number = num
+    return largest_number
 
 
 if __name__ == "__main__":
